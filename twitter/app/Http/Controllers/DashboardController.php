470a02a78bcabb5eccac294c56c0cdbd5d30f9ca
Ravenko123 <adam.vilagi14@gmail.com>
2024-10-03 12:04:14 +0200

    cscshodina19

--- twitter/app/Http/Controllers/DashboardController.php
+++ twitter/app/Http/Controllers/DashboardController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class DashboardController extends Controller
 { 
     public function index () {
+
+        Post::create([
+            'content' => "test123" , 
+            'likes'=> 15,
+        ]);
+
+        $posts->save();
         $users = [
             [
                 "name" => "Mario"
@@ -18,7 +26,8 @@ class DashboardController extends Controller
         return view(
             'dashboard',
             [
-                "users" => $users
+                "users" => $users,
+                "posts" => Post::orderBy('likes','ASC')->get(),
             ]
         );
     }
